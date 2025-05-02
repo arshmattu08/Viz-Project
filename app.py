f41118b4d69dc896d9bbf972b7043372f9f01412
arshmattu08 <singharshpreet6060@gmail.com>
2025-05-02 16:00:40 -0400

    added all viz and takeaway msgs

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-#imports
+# imports
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 
-from dash import Dash, html, dcc, Input, Output, callback, State
+from dash import Dash, html, dcc, Input, Output, callback, State, dash
 
 # Data 
 
@@ -11,8 +11,9 @@ url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSv5T0GjnV3amAPzMDv2hsgAL
 
 df = pd.read_csv(url)
 
-#visualization code
+# Visualization Code
 
+# Strength Distribution
 box_fig = px.box(df, x = 'sex', y = 'best3bench_kg', color = 'sex', hover_data= 'age')
 
 box_fig.update_layout(
@@ -22,6 +23,32 @@ box_fig.update_layout(
     xaxis_title = 'Sex',
     yaxis_title = 'Bench Press in KG'
 )
+
+
+# Bodyweight and Strength
+
+scatter_fig = px.scatter(df, x = 'bodyweight_kg', y = 'best3bench_kg')
+scatter_fig.update_layout(
+    template = 'simple_white',
+    # xaxis=dict(range=[0, power_data['bodyweight_kg'].max()]),
+    yaxis=dict(range=[0, df['best3bench_kg'].max()]),
+    xaxis_title = 'Bodyweight in KG',
+    yaxis_title = 'Benchpress in KG'
+
+)
+
+# Age and Strength
+
+fig = px.scatter(df, x = "age", y = "best3bench_kg", color = 'sex') # default color map seems to work well
+
+fig.update_layout(
+    plot_bgcolor='white',
+    xaxis=dict(range=[0, df['age'].max()]),
+    yaxis=dict(range=[0, df['best3bench_kg'].max()]),
+    title_text = 'Age vs bench press strength in men and women',
+    xaxis_title = 'Age',
+    yaxis_title = 'Bench Press in KG',
+    legend_title = 'Sex')
 
 
 # app code
@@ -38,7 +65,7 @@ app.layout = html.Div([
 
 # Sidebar
 html.Div([
-        html.Button("View Dataset",style={
+        html.Button("View Dataset",id = "view_data", style={
             "position":"relative","top":"100px",
             "padding":"12px 20px 12px 20px",
             "border-radius": "7px", 
@@ -78,7 +105,7 @@ html.Div([
      style={
         "padding": "20px",
         "width": "10%",
-        "backgroundColor": "#2E2E2E",
+        "backgroundColor": "#111e40",
         "height": "100vh",
         "position": "fixed",
         "top": 0,
@@ -87,7 +114,7 @@ html.Div([
 
 # Visualization Toggle Buttons
 html.Div([
-    html.Button("Strength Distribution", style = {
+    html.Button("Strength Distribution", id = "first-button",style = {
         "background-color":"transparent",
         "color":"white",
         "border-radius": "7px",
@@ -97,7 +124,7 @@ html.Div([
         "border": "2px solid white",
         "position":"relative","right":"8%"}),
     
-    html.Button("Bodyweight And Strength", style = {
+    html.Button("Bodyweight And Strength", id = "second-button",style = {
         "background-color":"transparent",
         "color":"white",
         "border-radius": "7px",
@@ -107,7 +134,7 @@ html.Div([
         "border": "2px solid white",
         "position":"relative","left":"5%"}),
 
-    html.Button("Age And Strength", style = {
+    html.Button("Age And Strength", id = "third-button",style = {
         "background-color":"transparent",
         "color":"white",
         "border-radius": "7px",
@@ -117,23 +144,61 @@ html.Div([
         "border": "2px solid white",
         "position":"relative","left":"20%"})]),
 
- # Visualization Space
-
+ # Visualization and Dataset Space
 html.Div([
-    dcc.Graph(id= "my-graph", figure = box_fig, style = {
-        "width":"45%",
-        "position":"relative","left":"34%",
-        "margin-top":"59px"
-        
-    })
+        dcc.Graph(id= "my-graph", style = {
+            "width":"60%",
+            "position":"relative","left":"28%",
+            "margin-top":"59px",}),
 
-]
-)
-
-
+        dcc.Markdown(id = "msg", style={"position":"relative","left":"100px","font-weight":'bold','color':'#6eb7ff'})
+    ])
 
 ], style = {"background-color":"#2E2E2E","color":"white","text-align":"center"}) # End of Outer Div
 
+
+# Callback Functionality
+
+# Strength distribution
+@callback(
+    Output('my-graph','figure'),
+    Output('msg','children'),
+    Input('first-button','n_clicks'),
+    prevent_initial_call='initial_duplicate')
+def show_box(n):
+    if n is not None and n > 0:
+        msg = '''Men have an higher median in bench press compared to women. Overall variation in men seem much higher    
+    as well in how much they lift.Both groups have several observations outside the IQR.
+        '''
+        return box_fig,msg
+    return dash.no_update,""
+    
+# Bodyweight and Strength
+@callback(
+    Output('my-graph','figure',allow_duplicate=True),
+    Output('msg','children', allow_duplicate=True),
+    Input('second-button','n_clicks'),
+    prevent_initial_call='initial_duplicate')
+def show_scatter(n):
+    if n is not None and n > 0:
+        msg = '''Benchpress tends to go up as the bodyweight increases of lifters.
+'''
+        return scatter_fig,msg
+    return dash.no_update,""
+
+
+# Age and Strength
+@callback(
+    Output('my-graph','figure',allow_duplicate=True),
+    Output('msg','children',allow_duplicate=True),
+    Input('third-button','n_clicks'),
+    prevent_initial_call='initial_duplicate')
+def show_scatter2(n):
+    if n is not None and n > 0:
+        msg = '''Age seems to play a key role in how much weight is lifted on bench press. It can be seen that  
+        after age of approx. 40, performance begins to decrease.'''
+        return fig,msg
+    return dash.no_update,""
 
 
 
