15e1559ed2133375b0636a5b51316fd1b6333632
arshmattu08 <singharshpreet6060@gmail.com>
2025-05-02 20:19:03 -0400

    two viz done for gender filter

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 
-from dash import Dash, html, dcc, Input, Output, callback, State, dash, dash_table
+from dash import Dash, html, dcc, Input, Output, callback, State, dash, dash_table, callback_context, exceptions
 
 # Data 
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSv5T0GjnV3amAPzMDv2hsgAL1P_XeQBK_mlBzxwy9XAXvgo-J6_CBGKoC0_0St0L2aFldI8ztEvZgD/pub?output=csv"
 
 df = pd.read_csv(url)
+df['date'] = pd.to_datetime(df['date'], format= "%m/%d/%y").dt.date
 
 # when viewing data, make sure no NA values show up
 df_cleaned = df.dropna()
@@ -28,19 +29,36 @@ box_fig.update_layout(
     xaxis_title = 'Sex',
     yaxis_title = 'Bench Press in KG'
 )
+box_fig.add_annotation(
+    x= 0.32,
+    y= 210,
+    text="Compare the variation in two groups",
+    showarrow=True,
+    arrowhead=2,
+    arrowwidth=1.5,
+    ax=75, ay= -30,
+    font=dict(size=13, color="black", weight = 'bold') )
 
 
 # Bodyweight and Strength
 
-scatter_fig = px.scatter(df, x = 'bodyweight_kg', y = 'best3bench_kg')
+scatter_fig = px.scatter(df, x = 'bodyweight_kg', y = 'best3bench_kg',color ='sex')
 scatter_fig.update_layout(
     template = 'simple_white',
     # xaxis=dict(range=[0, power_data['bodyweight_kg'].max()]),
     yaxis=dict(range=[0, df['best3bench_kg'].max()]),
     xaxis_title = 'Bodyweight in KG',
-    yaxis_title = 'Benchpress in KG'
+    yaxis_title = 'Benchpress in KG')
 
-)
+scatter_fig.add_annotation(
+    x= 90,
+    y= 360,
+    text="Clear rising trend in bodyweight and bench",
+    showarrow=True,
+    arrowhead=2,
+    arrowwidth=1.5,
+    ax=-60, ay= -30,
+    font=dict(size=11, color="black", weight = 'bold') )
 
 # Age and Strength
 
@@ -54,6 +72,17 @@ fig.update_layout(
     xaxis_title = 'Age',
     yaxis_title = 'Bench Press in KG',
     legend_title = 'Sex')
+
+
+fig.add_annotation(
+    x= 55,
+    y= 340,
+    text="Performance begins to decrease",
+    showarrow=True,
+    arrowhead=2,
+    arrowwidth=1.5,
+    ax=60, ay= -30,
+    font=dict(size=11, color="black", weight = 'bold'))
 
 
 # app code
@@ -87,22 +116,22 @@ html.Div([
             "font-size":"39px",
             "font-family":"Roboto, sans-serif"}),
 
-        dcc.Dropdown(options=[], placeholder="Year",style = {
+        dcc.Dropdown(id = "year-dropdown",options=[], placeholder="Year",style = {
             "color":"white",
             "position":"relative","top":"70px","left":"12px",
             "width":"88px"
             
         }),
-         dcc.Dropdown(options=[], placeholder="Age",style = {
+         dcc.Dropdown(id = "age-dropdown",options=[], placeholder="Age",style = {
             "color":"white",
             "position":"relative","top":"85px","left":"12px",
             "width":"88px"
             
         }),
-         dcc.Dropdown(options=[
+         dcc.Dropdown(id = "gender-dropdown",options=[
              {'label':'Male','value':'M'},
              {'label':'Female','value':'F'}
-         ], placeholder="Gender",style = {
+         ], placeholder="Gender", value = 'M',style = {
             "position":"relative","top":"100px","left":"12px",
             "width":"98px",
             "color":"black"})],
@@ -158,7 +187,9 @@ html.Div([
 
         dcc.Markdown(id = "msg", style={"position":"relative","left":"100px","font-weight":'bold','color':'#6eb7ff'}),
 
-        html.Div(id='data-table-container', style= {'display': 'none', 'padding': '30px', 'width': '80%', 'margin': 'auto'})
+        html.Div(id='data-table-container', style= {'display': 'none', 'padding': '30px', 'width': '80%', 'margin': 'auto'}),
+
+        dcc.Store(id="active-graph")
     ])
 
 ], style = {"background-color":"#2E2E2E","color":"white","text-align":"center"}) # End of Outer Div
@@ -170,13 +201,35 @@ html.Div([
 @callback(
     Output('my-graph','figure'),
     Output('msg','children'),
-    Input('first-button','n_clicks'),
+    Input('gender-dropdown','value'),
+    Input('active-graph','data'),
     prevent_initial_call='initial_duplicate')
-def show_box(n):
-    if n is not None and n > 0:
+def show_box(drop_value,active_graph):
+    df_filtered = df.loc[df['sex'] == drop_value,:]
+    box_fig = px.box(df_filtered, x = 'sex', y = 'best3bench_kg', color = 'sex', hover_data= 'age')
+
+    if active_graph == 'first-button':
+        box_fig.update_layout(
+        title = 'Distribution of upper body strength in men and women',
+        template = 'simple_white',
+        yaxis=dict(range=[0, df['best3bench_kg'].max()]),
+        xaxis_title = 'Sex',
+        yaxis_title = 'Bench Press in KG')
+
+        box_fig.add_annotation(
+        x= 0.32,
+        y= 210,
+        text="Compare the variation in two groups <br> using filter",
+        showarrow=True,
+        arrowhead=2,
+        arrowwidth=1.5,
+        ax=75, ay= -30,
+        font=dict(size=13, color="black", weight = 'bold') )
+
+        
         msg = '''Men have an higher median in bench press compared to women. Overall variation in men seem much higher    
-    as well in how much they lift.Both groups have several observations outside the IQR.
-        '''
+        as well in how much they lift.Both groups have several observations outside the IQR.
+            '''
         return box_fig,msg
     return dash.no_update,""
     
@@ -184,12 +237,34 @@ def show_box(n):
 @callback(
     Output('my-graph','figure',allow_duplicate=True),
     Output('msg','children', allow_duplicate=True),
-    Input('second-button','n_clicks'),
+    Input('gender-dropdown','value'),
+    Input('active-graph','data'),
     prevent_initial_call='initial_duplicate')
-def show_scatter(n):
-    if n is not None and n > 0:
+def show_scatter(drop_value,active_graph):
+    df_filtered = df.loc[df['sex'] == drop_value,:]
+
+    if active_graph == 'second-button':
+        scatter_fig = px.scatter(df_filtered, x = 'bodyweight_kg', y = 'best3bench_kg',color ='sex')
+        scatter_fig.update_layout(
+        template = 'simple_white',
+        # xaxis=dict(range=[0, power_data['bodyweight_kg'].max()]),
+        yaxis=dict(range=[0, df['best3bench_kg'].max()]),
+        xaxis_title = 'Bodyweight in KG',
+        yaxis_title = 'Benchpress in KG')
+
+        scatter_fig.add_annotation(
+        x= 90,
+        y= 360,
+        text="Clear rising trend in bodyweight and bench",
+        showarrow=True,
+        arrowhead=2,
+        arrowwidth=1.5,
+        ax=-60, ay= -30,
+        font=dict(size=11, color="black", weight = 'bold'))
+
+        
         msg = '''Benchpress tends to go up as the bodyweight increases of lifters.
-'''
+            '''
         return scatter_fig,msg
     return dash.no_update,""
 
@@ -215,7 +290,7 @@ def show_scatter2(n):
     Input('view_data', 'n_clicks'),
     prevent_initial_call=True)
 def display_table(n):
-    if n and n%2 != 0:
+    if n and n%2 != 0: # allow toggling
         table = dash_table.DataTable(
             data=df_sampled.to_dict('records'),
             columns=[{"name": str(col), "id": str(col)} for col in df_sampled.columns],
@@ -228,5 +303,26 @@ def display_table(n):
         return table, {'display': 'block'}
     return dash.no_update, {'display': 'none'}
 
+# Storing last button clicked data, this will help in filters.
+@callback(
+    Output('active-graph', 'data'),
+    Input('first-button', 'n_clicks'),
+    Input('second-button', 'n_clicks'),
+    Input('third-button', 'n_clicks'),
+    prevent_initial_call=True
+)
+def update_active_graph(n1, n2, n3):
+    ctx = callback_context
+    if not ctx.triggered:
+        raise exceptions.PreventUpdate
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    return button_id
+
+
+
+
 if (__name__ == '__main__'):
     app.run(debug= True)
+
+
+
